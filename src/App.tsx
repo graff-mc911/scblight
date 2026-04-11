b1593ad6edfcbca41af03bb0129e9e9555f5c920
graff-mc911 <125906318+graff-mc911@users.noreply.github.com>
2026-04-11 16:45:08 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,9 @@ const InvoiceView = lazy(() => import('./pages/InvoiceView'));
 const ClientForm = lazy(() => import('./pages/ClientForm'));
 const Onboarding = lazy(() => import('./pages/Onboarding'));
 const ClientInvoices = lazy(() => import('./pages/ClientInvoices'));
+const ClientInvoices = lazy(() => import('./pages/ClientInvoices'));
   import('./pages/ClientInvoices').then((module) => ({ default: module.ClientInvoices }))
-
+);
 const ReceiptForm = lazy(() => import('./pages/ReceiptForm'));
 const PdfCreator = lazy(() => import('./pages/PdfCreator'));
 
